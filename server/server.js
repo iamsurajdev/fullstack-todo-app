b509084c85d50c5d305c5338272a97a41412a7a8
iamsurajdev <surajbiswas367@gmail.com>
2024-07-05 23:39:20 +0530

    basic server setup and db connection added

---
+++ server/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+require("dotenv").config();
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+const corsOptions = {
+  origin: "*",
+};
+app.use(cors(corsOptions));
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+
+// Connecting with the DB
+mongoose.connect(process.env.DB_CONNECTION_URL);
+
+//create a server object:
+app.listen(PORT, () => {
+  console.log("starting the server");
+});
